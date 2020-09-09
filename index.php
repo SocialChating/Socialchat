c78c5344a2cc0b972e8adc21c3b3543ef36feea8
Patel Raj <patel.rajs411@gmail.com>
2020-09-09 19:27:07 +0530

    index rename

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php // index.php
-include_once 'header.php';
-echo "<br /><span class='main'>Welcome to Social's Nest,";
-if ($loggedin) echo " $user, you are logged in.";
-else echo ' Please sign up and/or log in to join in.';
-?>
-</span><br /><br /></body></html>
