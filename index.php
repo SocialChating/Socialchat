b6190bd12fb23e518f4d1e180f0060d2915b13c0
Patel Raj <patel.rajs411@gmail.com>
2020-09-09 18:57:46 +0530

    change

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 include_once 'header.php';
 echo "<br /><span class='main'>Welcome to Social's Nest,";
 if ($loggedin) echo " $user, you are logged in.";
-else echo ' please sign up and/or log in to join in.';
+else echo ' Please sign up and/or log in to join in.';
 ?>
 </span><br /><br /></body></html>
